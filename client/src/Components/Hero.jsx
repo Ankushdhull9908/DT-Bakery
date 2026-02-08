32b86053c66ba0b3933eea445fdeb492e3542a25
Ankushdhull9908 <ankushdhull9908@gmail.com>
2026-02-08 17:19:59 +0530

    made hero responsive

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -7,7 +7,21 @@ function Hero() {
     <div className='hero'>
         <img src={icons.heroimg} alt='heroimage'/>
         <div className="herocontent">
-            <p>EnjoY Delicious Food</p>
+            <div className="heroheading">
+              <h1>Chocolate Cake</h1>
+            </div>
+            <div className="heropara">
+             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis debitis mollitia explicabo quae consectetur, ipsam fugiat dignissimos sapiente repellendus at harum deserunt corporis ab rerum tenetur nesciunt odit inventore numquam.</p>
+            </div>
+            <div className="herobuttoncollection">
+              <div className="herobtn">
+                <p>SHOP NOW</p>
+              </div>
+              <div className="herobtn">
+                <p>VIEW COLLECTION</p>
+              </div>
+            </div>
+
 
         </div>
       
